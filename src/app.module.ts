00efd1cfcb941ca4f5857b8aac9077e980bc8ad0
zx <zxpstudy@163.com>
2020-07-01 17:25:30 +0800

    feat: add middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,16 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { LoggerMiddleware } from './middlewares/logger.middleware';
 
 @Module({
   imports: [UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule{
+  configure(consumer: MiddlewareConsumer): any {
+    consumer.apply(LoggerMiddleware).forRoutes('users');
+  }
+}
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, HttpStatus, Post, Response,Param,Body } from '@nestjs/common';
+import { Controller, Get, HttpStatus, Post, Response, Param, Body, HttpException } from '@nestjs/common';
 import {CreateUserDto} from './dto/CreateUserDto';
 import { UsersService } from './users.service';
 
@@ -11,29 +11,26 @@ export class UsersController {
 
   @Get()
   async getAllUsers(@Response() res){
-    console.log('get users')
     try {
       const users = await this.usersService.getAllUsers();
        res.status(HttpStatus.OK).json(users);
-    } catch (e) {
-      res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({});;
-    }
-  }
-
-  @Get('/:id')
-  async getUser(@Param('id') id,@Response() res){
-    console.log('get user',id);
-    try {
-      const user = await this.usersService.getUser(id);
-      res.status(HttpStatus.OK).json(user);
     } catch (e) {
       res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({});
     }
   }
 
+  @Get('/:id')
+  async getUser(@Param('id') id,@Response() res){
+    try {
+      const user = await this.usersService.getUser(id);
+      res.status(HttpStatus.OK).json(user);
+    } catch (e) {
+      throw new HttpException('user not found',HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+  }
+
   @Post()
   addUser(@Response() res,@Body() createUserDto:CreateUserDto){
-    console.log('add users',createUserDto)
     this.usersService.addUser(createUserDto);
     res.status(HttpStatus.OK).json({});
   }
