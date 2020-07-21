3a77a3881f81fcf12ef117b26fd4f10d7d4374eb
zx <zxpstudy@163.com>
2020-07-21 19:39:42 +0800

    fix: add user mock data

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,27 +1,26 @@
-import { HttpException, Injectable } from '@nestjs/common';
-import {filter} from 'lodash';
-import { CreateUserDto } from './dto/create-user.dto';
+import { HttpException, Injectable } from "@nestjs/common";
+import { filter, times, map } from "lodash";
+import { CreateUserDto } from "./dto/create-user.dto";
 
 @Injectable()
 export class UsersService {
-  private users:CreateUserDto[]=[
-    {id:0,name:'zx',age:'20'},
-    {id:1,name:'xiao',age:'1'}
-  ];
-  getAllUsers(){
+  private users: CreateUserDto[] = map(times(10), (_, index) => {
+    return { id: index, name: "user" + index, age: index * 10 + "" };
+  });
+  getAllUsers() {
     return Promise.resolve(this.users);
   }
-  getUser(id:number){
-    const users = filter(this.users,(u)=>{
+  getUser(id: number) {
+    const users = filter(this.users, (u) => {
       return u.id === id;
     });
-    if(!users || users.length<=0){
-      throw new HttpException('user not found',404);
+    if (!users || users.length <= 0) {
+      throw new HttpException("user not found", 404);
     }
 
     return Promise.resolve(users[0]);
   }
-  addUser(user:CreateUserDto){
+  addUser(user: CreateUserDto) {
     this.users.push(user);
   }
 }
